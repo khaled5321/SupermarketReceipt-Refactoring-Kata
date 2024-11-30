74b736018c928a6d03bd42f01e37b2ac7c951f8f
Khaled Emad <khaledemad656@gmail.com>
2024-11-30 15:33:56 +0200

    Refactor Tests to use unittest

--- python/tests/test_supermarket.py
+++ python/tests/test_supermarket.py
@@ -1,4 +1,4 @@
-import pytest
+import unittest
 
 from model_objects import Product, SpecialOfferType, ProductUnit
 from shopping_cart import ShoppingCart
@@ -6,27 +6,33 @@ from teller import Teller
 from tests.fake_catalog import FakeCatalog
 
 
-def test_ten_percent_discount():
-    catalog = FakeCatalog()
-    toothbrush = Product("toothbrush", ProductUnit.EACH)
-    catalog.add_product(toothbrush, 0.99)
+class TestSuperMarket(unittest.TestCase):
+    def setUp(self):
+        # test products
+        self.toothbrush = Product("toothbrush", ProductUnit.EACH)
+        self.apples = Product("apples", ProductUnit.KILO)
 
-    apples = Product("apples", ProductUnit.KILO)
-    catalog.add_product(apples, 1.99)
+        self.catalog = FakeCatalog()
+        self.catalog.add_product(self.toothbrush, 0.99)
+        self.catalog.add_product(self.apples, 1.99)
 
-    teller = Teller(catalog)
-    teller.add_special_offer(SpecialOfferType.TEN_PERCENT_DISCOUNT, toothbrush, 10.0)
+    def test_ten_percent_discount(self):
+        teller = Teller(self.catalog)
+        teller.add_special_offer(
+            SpecialOfferType.TEN_PERCENT_DISCOUNT, self.toothbrush, 10.0
+        )
 
-    cart = ShoppingCart()
-    cart.add_item_quantity(apples, 2.5)
+        cart = ShoppingCart()
+        cart.add_item_quantity(self.apples, 2.5)
 
-    receipt = teller.checks_out_articles_from(cart)
+        receipt = teller.checks_out_articles_from(cart)
 
-    assert 4.975 == pytest.approx(receipt.total_price(), 0.01)
-    assert [] == receipt.discounts
-    assert 1 == len(receipt.items)
-    receipt_item = receipt.items[0]
-    assert apples == receipt_item.product
-    assert 1.99 == receipt_item.price
-    assert 2.5 * 1.99 == pytest.approx(receipt_item.total_price, 0.01)
-    assert 2.5 == receipt_item.quantity
+        self.assertAlmostEqual(4.975, receipt.total_price())
+        self.assertEqual(receipt.discounts, [])
+        self.assertEqual(1, len(receipt.items))
+
+        receipt_item = receipt.items[0]
+        self.assertEqual(receipt_item.product, self.apples)
+        self.assertEqual(receipt_item.price, 1.99)
+        self.assertEqual(receipt_item.total_price, 2.5 * 1.99)
+        self.assertEqual(receipt_item.quantity, 2.5)
